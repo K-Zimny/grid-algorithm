af86652f838b121e806c0ebfae6137dcc1c41cb8
Ken Zimny <kenneth.zimny@gmail.com>
2024-08-18 19:38:36 -0700

    Cleans up project.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Board from "@/app/components/Board";
 export default function Home() {
   return (
     <div className="App">
-      <h1>Mine Sweeper</h1>
       <Board></Board>
     </div>
   );
